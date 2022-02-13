7c36c17821bb1f0310c3a76311f34e92f329f514
Eugene Brodsky <fupslot@gmail.com>
2022-02-13 20:57:38 +0200

    align navitation items

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,20 +3,21 @@ import { Link } from "gatsby"
 
 const Menu = () => {
     return (
-    <nav className="flex justify-around py-4">
-        <div className="basis-1/4">
+    <nav className="flex justify-between py-4">
+        <div className="">
             <Link to="/">
             <h3 className="text-2xl">Safe Space</h3>
             </Link>
         </div>
-        <div className="flex justify-evenly space-x-2">
-            <div className="flex justify-center px-4 py-2 bg-pink-200 rounded-md hover:bg-pink-300 hover:text-pink-100 text-pink-700 shadow-md transition-colors duration-100">
+        <div className="flex justify-evenly items-scenter space-x-2">
+            <div className="flex px-4 py-2 bg-pink-200 rounded-md hover:bg-pink-300 hover:text-pink-100 text-pink-700 shadow-md transition-colors duration-100">
                 <Link to="/product">Product</Link>
             </div>
             {/* <div className="flex justify-center px-4 py-2 bg-pink-200 rounded-md hover:bg-pink-300 hover:text-pink-100 text-pink-700 shadow-md transition-colors duration-100">
                 <Link to="/about">About</Link>
             </div> */}
         </div>
+        <div />
     </nav>
     )
 }
